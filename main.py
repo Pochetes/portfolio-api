14981d882b89a4558f425be344072328d39ab582
Pochetes <robertomiguel2001@gmail.com>
2021-12-14 18:53:54 -0500

    removed unnecessary asyncio stuff | testing out first models

--- main.py
+++ main.py
@@ -4,23 +4,15 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
 
-# database connection status handling
-async def get_server_info():
+load_dotenv()
+MONGODB_URI = os.environ["MONGODB_URI"]
 
-    load_dotenv()
-    MONGODB_URI = os.environ["MONGODB_URI"]
-
-    # instantiation of database object
-    client = AsyncIOMotorClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
-
-    try:
-        print("Connected to server successfully.")
-    except Exception:
-        print("Unable to connect to the server.")
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(get_server_info())
+# instantiation of database object
+client = AsyncIOMotorClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
+db = client.person
 
 app = FastAPI()
 
@@ -39,6 +31,47 @@ class PyObjectId(ObjectId):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
+
+# model for the GET /personal/info endpoint
+class PersonalInfoModel(BaseModel):
+    firstName: str
+    lastName: str
+    email: EmailStr
+    description: str
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = { ObjectId: str }
+        # schema (response example) for the GET /personal/info endpoint
+        schema_extra = {
+            "example": {
+                "firstName": "John",
+                "lastName": "Doe",
+                "email": "johndoe@gmail.com",
+                "description": "What a nice guy!"
+            }
+        }
+
+# model for the PUT /personal/info endpoint
+class UpdatePersonalInfoModel(BaseModel):
+    firstName: Optional[str]
+    lastName: Optional[str]
+    email: Optional[EmailStr]
+    description: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = { ObjectId: str }
+        # schema (response example) for the PUT /personal/info endpoint
+        schema_extra = {
+            "example": {
+                "firstName": "John",
+                "lastName": "Doe",
+                "email": "johndoe@gmail.com",
+                "description": "What a nice guy!"
+            }
+        }
 
 @app.get('/')
 async def hello():
