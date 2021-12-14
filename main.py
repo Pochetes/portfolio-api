1403791da0e5d9756a7d982823feff6bed2b17ae
Pochetes <robertomiguel2001@gmail.com>
2021-12-14 17:58:40 -0500

    added compatibility between BSON and JSON strings btwn MongoDB and FastAPI

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import asyncio
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
 
+# database connection status handling
 async def get_server_info():
 
     load_dotenv()
     MONGODB_URI = os.environ["MONGODB_URI"]
 
+    # instantiation of database object
     client = AsyncIOMotorClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
 
     try:
@@ -20,6 +23,22 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(get_server_info())
 
 app = FastAPI()
+
+# helps convert id data type into string for FastAPI
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 
 @app.get('/')
 async def hello():
