a449eeb46f72167a73b810bfb0ac8ce060269d98
Pochetes <robertomiguel2001@gmail.com>
2021-12-15 00:06:49 -0500

    added CORS middleware handler

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
-import asyncio
-from fastapi import FastAPI
+from pymongo import MongoClient
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from bson import ObjectId
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional, List
 
 load_dotenv()
@@ -15,6 +17,29 @@ client = AsyncIOMotorClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
 db = client.person
 
 app = FastAPI()
+
+# setup CORS middleware
+origins = [
+    "http://pochetes.herokuapp.com/personal",
+    "http://pochetes.herokuapp.com/experiences",
+    "http://pochetes.herokuapp.com/projects",
+    "https://pochetes.herokuapp.com/personal",
+    "https://pochetes.herokuapp.com/experiences",
+    "https://pochetes.herokuapp.com/projects",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8000/personal",
+    "http://localhost:8000/experiences",
+    "http://localhost:8000/projects",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # helps convert id data type into string for FastAPI
 class PyObjectId(ObjectId):
