f92090afd9c0bce16b48a2f8c2a1cde224abfc82
Pochetes <robertomiguel2001@gmail.com>
2021-12-17 04:35:56 -0500

    resolved escaped double quotes JSON response, now prettified

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
-import os
-import json
+import os, json
 from models import InfoModel
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -17,17 +16,26 @@ personCol = db.person
 # GET
 def getPerson():
     person = list(personCol.find({}))
-    return json.dumps(person, default=json_util.default)
+    # converts list object to str (JSON format)
+    personToStr = json.dumps(person, default=json_util.default)
+    # converts str obj to JSON type (avoids escape double quotes)
+    return json.loads(personToStr)
 
 # POST
 def createPerson(info: InfoModel): # CAN ONLY BE DONE ONCE
     person = info
     personCol.insert_one(person)
-    return json.dumps(person, default=json_util.default)
+    # converts list object to str (JSON format)
+    personToStr = json.dumps(person, default=json_util.default)
+    # converts str obj to JSON type (avoids escape double quotes)
+    return json.loads(personToStr)
 
 # PUT
 def updatePerson(firstName: str, info: InfoModel):
     person = info
     personCol.update_one({"firstName": firstName}, { "$set": person})
     personCol.find_one({"firstName": firstName})
-    return json.dumps(person, default=json_util.default)
+    # converts list object to str (JSON format)
+    personToStr = json.dumps(person, default=json_util.default)
+    # converts str obj to JSON type (avoids escape double quotes)
+    return json.loads(personToStr)
