f9d9667726d6900019f8df15fcda28416f9c3bf5
Pochetes <robertomiguel2001@gmail.com>
2021-12-18 18:02:34 -0500

    separated user endpoint into its own package w/ models and routers

---
+++ endpoints/user/models.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional
+from bson import ObjectId
+
+# Converting ObjectId (BSON) into str for JSON
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+# ========= /user/contacts Endpoint =========
+class Contact(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    title: str = Field(...)
+    link: str = Field(...)
+
+class UpdateContact(BaseModel):
+    title: Optional[str]
+    link: Optional[str]
+# ========= END /user/contacts Endpoint =========
+
+# ========= /user Endpoint =========
+class User(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    firstName: str = Field(...)
+    lastName: str = Field(...)
+    email: EmailStr = Field(...)
+    description: str = Field(...)
+    contacts: List[Contact] = []
+
+class UpdateUser(BaseModel):
+    firstName: Optional[str]
+    lastName: Optional[str]
+    email: Optional[EmailStr]
+    description: Optional[str]
+    contacts: Optional[List[Contact]]
+# ========= END /user Endpoint =========
