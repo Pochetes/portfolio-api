a9271bd54a0ab4021d1a3702235a638fea5b6464
Pochetes <robertomiguel2001@gmail.com>
2021-12-19 15:42:16 -0500

    added experiences schema to hide ObjectId

--- endpoints/experiences/models.py
+++ endpoints/experiences/models.py
@@ -20,12 +20,26 @@ class PyObjectId(ObjectId):
 
 # ========= /experience Endpoint =========
 class Experience(BaseModel):
-    # id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     company: str = Field(...)
     position: str = Field(...)
     dateStarted: str = Field(...)
     dateEnded: str = Field(...)
     image: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "company": "Meta Platforms Inc.",
+                "position": "Software Engineer Intern",
+                "dateStarted": "June 2021",
+                "dateEnded": "August 2021",
+                "image": "<link_to_image>"
+            }
+        }
 
 class UpdateExperience(BaseModel):
     company: Optional[str]
@@ -33,4 +47,17 @@ class UpdateExperience(BaseModel):
     dateStarted: Optional[str]
     dateEnded: Optional[str]
     image: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "company": "Meta Platforms Inc.",
+                "position": "Software Engineer Intern",
+                "dateStarted": "June 2021",
+                "dateEnded": "August 2021",
+                "image": "<link_to_image>"
+            }
+        }
 # ========= END /experience Endpoint =========
