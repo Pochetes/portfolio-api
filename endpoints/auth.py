f749d1da488d57d6c4ea6799e7684abb01092181
Pochetes <robertomiguel2001@gmail.com>
2021-12-25 08:01:39 -0500

    added authentication function to implement

---
+++ endpoints/auth.py
@@ -0,0 +1,16 @@
+from fastapi import Depends
+from fastapi.exceptions import HTTPException
+from fastapi.security import HTTPBearer
+
+from .utils import VerifyToken
+
+# Scheme for the Authorization header
+tokenAuthScheme = HTTPBearer()
+
+def has_access(token: str = Depends(tokenAuthScheme)):
+
+    result = VerifyToken(token.credentials).verify()
+
+    if result.get("status"):
+        # response.status_code = status.HTTP_400_BAD_REQUEST
+        raise HTTPException(401, result.get("message"))
