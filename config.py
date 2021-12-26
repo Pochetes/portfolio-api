b72095bb902a61616126001a6778852eff585db7
Pochetes <robertomiguel2001@gmail.com>
2021-12-26 04:41:16 -0500

    changed HOST address

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings
 
 # CHANGE
 class ServerSettings(BaseSettings):
-    HOST: str = "127.0.0.1"
+    HOST: str = "0.0.0.0"
     PORT: int = 8080
     LOG_LEVEL: str = "info"
 
