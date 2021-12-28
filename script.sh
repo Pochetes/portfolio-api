6ad80c813c8da64dc051fa4901fa64e6e0206161
Pochetes <robertomiguel2001@gmail.com>
2021-12-28 05:44:39 -0500

    adding procfile and final script setup for app

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #!bin/bash
 
+# prod Procfile
+# gunicorn main:app --access-logfile - --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
+
 # dev
 # pipenv run python main.py
 
-# prod
-gunicorn main:app --access-logfile - --worker-tmp-dir /dev/shm --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080
+# prod - Docker
+gunicorn main:app --access-logfile - --worker-tmp-dir /dev/shm --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
